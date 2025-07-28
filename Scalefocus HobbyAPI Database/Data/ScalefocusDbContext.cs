581e628f5ea96aaa40acc9e86c6c09d48728569a
ZaferZ <zafer.bayraktarov@gmail.com>
2025-07-28 15:54:03 +0300

    Added DbContext

---
+++ Scalefocus HobbyAPI Database/Data/ScalefocusDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Scalefocus_HobbyAPI_Database.Models;
+
+namespace Scalefocus_HobbyAPI_Database.Data
+{
+    public class ScalefocusDbContext(DbContextOptions<ScalefocusDbContext> options) : DbContext(options)
+    {
+        public DbSet<Event> Events { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+}
