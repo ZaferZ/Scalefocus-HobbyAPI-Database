6530bc3f186c2b11936b6d37219d8729d3030fd2
ZaferZ <zafer.bayraktarov@gmail.com>
2025-07-28 15:35:25 +0300

    Added Event entity

---
+++ Scalefocus HobbyAPI Database/Models/Event.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace Scalefocus_HobbyAPI_Database.Models
+{
+        public enum EventStatus
+        {
+            Scheduled = 0,
+            Cancelled = 1,
+            Completed = 2
+        }
+        public class Event
+        {
+            [Key]
+            public Guid Id { get; set; }
+            [Required]
+            public string? Title { get; set; }
+            [Required]
+            public string? Description { get; set; }
+            [Required]
+            public Guid OwnerId { get; set; }
+            [Required]
+            public Guid HobbyId { get; set; }
+            [Required]
+            public DateTime StartDate { get; set; }
+            [Required]
+            public DateTime EndDate { get; set; }
+            [Required]
+            public string? Location { get; set; }
+            [Required]
+            public int Capacity { get; set; }
+            [Required]
+            public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+            [Required]
+            public Guid CreatedBy { get; set; }
+            public DateTime? ModifiedAt { get; set; }
+            public Guid? ModifiedBy { get; set; }
+            public List<Guid>? ParticipantIds { get; set; } = new();
+            [Required]
+            public EventStatus Status { get; set; } = EventStatus.Scheduled;
+
+        }
+    }
+
+
