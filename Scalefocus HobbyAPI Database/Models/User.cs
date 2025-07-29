94a12dcddecd133bf73b0abb152ff5ab98596037
Velislav Shopov <shopov.velislav@gmail.com>
2025-07-29 11:17:17 +0300

    Added properties to user

--- Scalefocus HobbyAPI Database/Models/User.cs
+++ Scalefocus HobbyAPI Database/Models/User.cs
@@ -1,6 +1,20 @@
-﻿namespace Scalefocus_HobbyAPI_Database.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Scalefocus_HobbyAPI_Database.Models
 {
     public class User
     {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get;set; }
+
+        [Required]
+        public string Email { get; set; }
+
     }
 }
