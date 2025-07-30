00d47cf707628465f1561eb6fa5cc1432ad7eac7
ZaferZ <zafer.bayraktarov@gmail.com>
2025-07-30 15:22:20 +0300

    Added tasks

--- Scalefocus HobbyAPI Database/Data/ScalefocusDbContext.cs
+++ Scalefocus HobbyAPI Database/Data/ScalefocusDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CommentAPI.Entities;
+using Microsoft.EntityFrameworkCore;
 using Scalefocus_HobbyAPI_Database.Models;
 
 namespace Scalefocus_HobbyAPI_Database.Data
@@ -11,6 +12,9 @@ namespace Scalefocus_HobbyAPI_Database.Data
 
         public DbSet<Hobbies> Hobbies { get; set; }
 
+        public DbSet<CommentEntity> Comments { get; set; }
+
+        public DbSet<TaskEntity> Tasks { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
---
+++ Scalefocus HobbyAPI Database/Models/TaskEntity.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Scalefocus_HobbyAPI_Database.Models
+{
+    public class TaskEntity
+    {
+        [Key]
+        public int TaskId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        
+        public string Description { get; set; }
+        [Required]
+        public bool IsCompleted { get; set; } = false;
+        
+        public DateTime ModifiedAt { get; set; }
+     
+    }
+}
