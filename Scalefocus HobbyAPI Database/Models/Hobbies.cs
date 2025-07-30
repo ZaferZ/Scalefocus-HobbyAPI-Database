ec13491145aef046555d3fe96cd638360f747456
kahuwa <105742994+kahuwa@users.noreply.github.com>
2025-07-30 11:01:31 +0300

    Added Hobbies Entitiy and DBset

---
+++ Scalefocus HobbyAPI Database/Models/Hobbies.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Scalefocus_HobbyAPI_Database.Models
+{
+    public class Hobbies
+    {
+        [Key]
+        [Required]
+        public int type_id { get; set; }
+        [Required]
+        public string title { get; set; }
+        [Required]
+        public DateTime date { get; set; }
+        [Required]
+        public int updated_at { get; set; }
+    }
+}
