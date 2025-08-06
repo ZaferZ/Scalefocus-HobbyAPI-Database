442dd8477ff14a188ca7bc6e022ce9093fd2a25b
Velislav Shopov <shopov.velislav@gmail.com>
2025-08-06 16:27:43 +0300

    Added refreshtokens and userroles dbsets, added migrations for them

--- Scalefocus HobbyAPI Database/Migrations/20250806132409_changed_userroles.Designer.cs
+++ Scalefocus HobbyAPI Database/Migrations/20250806132409_changed_userroles.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Scalefocus_HobbyAPI_Database.Data;
 
@@ -11,9 +12,11 @@ using Scalefocus_HobbyAPI_Database.Data;
 namespace Scalefocus_HobbyAPI_Database.Migrations
 {
     [DbContext(typeof(ScalefocusDbContext))]
-    partial class ScalefocusDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250806132409_changed_userroles")]
+    partial class changed_userroles
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -452,7 +455,7 @@ namespace Scalefocus_HobbyAPI_Database.Migrations
                     b.HasIndex("UserId")
                         .IsUnique();
 
-                    b.ToTable("RefreshTokens");
+                    b.ToTable("RefreshToken");
                 });
 
             modelBuilder.Entity("Scalefocus_HobbyAPI_Database.Models.Role", b =>
